19eba6b7b974dc22121a59c402896ba47c8c9536
Spidersbox <Spider@Spidersbox.com>
2016-03-20 00:21:00 -0700

    first commit

---
+++ Bitbar.cpp
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+USEFORM("BitbarMain.cpp", BitbarForm);
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+        try
+        {
+                 Application->Initialize();
+                 Application->CreateForm(__classid(TBitbarForm), &BitbarForm);
+                 Application->Run();
+        }
+        catch (Exception &exception)
+        {
+                 Application->ShowException(&exception);
+        }
+        catch (...)
+        {
+                 try
+                 {
+                         throw Exception("");
+                 }
+                 catch (Exception &exception)
+                 {
+                         Application->ShowException(&exception);
+                 }
+        }
+        return 0;
+}
+//---------------------------------------------------------------------------
---
+++ BitbarMain.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef BitbarMainH
+#define BitbarMainH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TBitbarForm : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+        __fastcall TBitbarForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TBitbarForm *BitbarForm;
+//---------------------------------------------------------------------------
+#endif
